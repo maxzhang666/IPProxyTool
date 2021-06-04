81809cbb568de602cd83fce7019d72c1681b4564
Max zhang <373884384@qq.com>
2021-06-04 15:24:59 +0800

    task done

--- run_test.py
+++ run_test.py
@@ -1,37 +1,58 @@
-import logging
+import logging.config
 import os
 import re
 import sys
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 import requests
-import logging.config
-
-from crawler.spiders.proxy.sixsixip import SixSixIpSpider
-from sql import SqlManager
-
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # 禁用安全请求警告
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+logging_path = os.path.split(
+    os.path.realpath(__file__))[0] + os.sep + 'logging.conf'
+logging.config.fileConfig(logging_path)
+logger = logging.getLogger('gogogo')
 
 
-def local_go(code):
-    ip_handle = open('./ip.txt')
+class TaskThread():
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:50.0) Gecko/20100101 Firefox/50.0',
-        'origin': 'https://greasyfork.org'
-    }
+    def __init__(self, num: int = 10):
+        # 线程池+线程同步改造添加代码处1/5： 定义锁和线程池
+        # 我们第二大节中说的是锁不能在线程类内部实例化，这个是调用类不是线程类，所以可以在这里实例化
+        self.threadLock = threading.Lock()
+        # 定义2个线程的线程池
+        self.thread_pool = ThreadPoolExecutor(num)
+        # 定义2个进程的进程池。进程池没用写在这里只想表示进程池的用法和线程池基本一样
+        # self.process_pool = ProcessPoolExecutor(2)
+        pass
 
-    lines = ip_handle.readlines()
-    for item in lines:
+    def main_logic(self, ips, codes):
+
+        for ip in ips:
+            for code in codes:
+                self.call_do_something({'proxy': {'http': ip}, 'code': code})
+
+    # 线程池+线程同步改造添加代码处2/5： 添加一个通过线程池调用do_something的中间方法。参数与do_something一致
+    def call_do_something(self, para):
+        self.thread_pool.submit(self.do_request, para)
+
+    def do_request(self, para):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:50.0) Gecko/20100101 Firefox/50.0',
+            'origin': 'https://greasyfork.org'
+        }
+
         try:
+            start_time = time.perf_counter()
             session = requests.session()
             session.verify = None
             session.headers.update(headers)
-            session.proxies = {'http': item}
+            session.proxies = para['proxy']
 
-            response = session.get('https://greasyfork.org/zh-CN/scripts/{}'.format(code))
+            response = session.get('https://greasyfork.org/zh-CN/scripts/{}'.format(para['code']))
             # 二次访问 获取authcode
             response = session.get(response.url)
 
@@ -41,15 +62,13 @@ def local_go(code):
             post_url = "https://greasyfork.org{}".format(infos[0])
 
             post_response = session.post(post_url, {})
-            logger.info('代理{},请求结果:{}({})'.format(item, '成功' if post_response.status_code == 204 else '失败', post_response.status_code))
+            stop_time = time.perf_counter()
+            cost = stop_time - start_time
+            logger.info('代理{},请求结果:{}({});耗时:{}秒'.format(para, '成功' if post_response.status_code == 204 else '失败', post_response.status_code, cost))
         except Exception as e:
             logger.error(e)
 
 
-logging_path = os.path.split(
-    os.path.realpath(__file__))[0] + os.sep + 'logging.conf'
-logging.config.fileConfig(logging_path)
-logger = logging.getLogger('gogogo')
 if __name__ == '__main__':
     # sql = SqlManager()
 
@@ -57,4 +76,8 @@ if __name__ == '__main__':
     code = sys.argv[2] or '384538'
 
     if go_type == 'local':
-        local_go(code)
+        ip_handle = open('./ip.txt')
+        ips = ip_handle.readlines()
+        task = TaskThread(100)
+        task.main_logic(ips, [code])
+    logger.info('任务生成完成')
